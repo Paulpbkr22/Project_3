5ccb668abfea9ae890f7c6665a8adfa9eae95a2b
Paul Baker <paulpbkr22@gmail.com>
2020-02-19 19:16:49 -0500

    first-push

---
+++ routes/api/drinks.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const drinksController = require("../../controllers/drinksController");
+
+// Matches with "/api/drinks"
+router.route("/")
+  .get(drinksController.findAll)
+  // .post(booksController.create, function(req, res) {});
+  router.post("/",function(req, res){
+    console.log(req.body)
+    drinksController.create(req, res)
+  });
+
+// Matches with "/api/drinks/:id"
+router
+  .route("/:id")
+  .get(drinksController.findById)
+  .put(drinksController.update)
+  .delete(drinksController.remove);
+
+module.exports = router;
